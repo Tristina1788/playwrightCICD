6686be66f704b3551ed12efb37c11a99f1149224
Tristina <ttmnguyen@gosecure.net>
2024-04-24 13:11:19 +0700

    update test for API

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
-
+  await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
